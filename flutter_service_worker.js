00c1d91fe68cbafca9acf9fa9726dadb5f077a48
Linda TJ <sameer.tuteja05@gmail.com>
2020-09-22 21:03:51 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,13 +6,13 @@ const RESOURCES = {
   "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "45cbfc992fcc8572f43ea256ce51db13",
+"assets/NOTICES": "729c5330545dfe2ba5fa147b25890e43",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "0be213362d87eee231857ee922ed7518",
 "/": "0be213362d87eee231857ee922ed7518",
-"main.dart.js": "4b70a227acee39cdf27f842ab6bef0d7",
+"main.dart.js": "b2a7e2690de2ffcecc90bd81315848b5",
 "manifest.json": "1d01cc00b30be88149f52bd312d5ea39"
 };
 
