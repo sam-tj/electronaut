6286245aa5f17b03100a3333c39280cde4688565
Linda TJ <sameer.tuteja05@gmail.com>
2020-09-24 16:52:28 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "3600f98cdcd88bfd1900dd50f41256fe",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "48f3c1364e7c6c5021e79032937ece1b",
+"assets/NOTICES": "4c3f6cb2cebae9bdd4b564fa29ea39b5",
 "assets/packages/flutter_inappwebview/t_rex_runner/t-rex.css": "5a8d0222407e388155d7d1395a75d5b9",
 "assets/packages/flutter_inappwebview/t_rex_runner/t-rex.html": "16911fcc170c8af1c5457940bd0bf055",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "0be213362d87eee231857ee922ed7518",
 "/": "0be213362d87eee231857ee922ed7518",
-"main.dart.js": "e805b6eaef7a03bd83e86484768d3e79",
+"main.dart.js": "2ee4b35571b8745dea937f75466de2e4",
 "manifest.json": "1d01cc00b30be88149f52bd312d5ea39"
 };
 
